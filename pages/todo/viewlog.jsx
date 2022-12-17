c7d7afa13edc1f5eb8bffd70c4f3c37ef8db4c8a
Ajithkumar J <73721580+ak6671@users.noreply.github.com>
2022-12-17 11:34:29 +0530

    1

--- pages/todo/viewlog.jsx
+++ pages/todo/viewlog.jsx
@@ -1,26 +1,24 @@
-import { redis } from "../api/lib/lib"
+// import { redis } from "../api/lib/lib"
 
-export async function getServerSideProps(context) {
-  const entries = await redis.lrange("todo_log", 0, -1);
-  return {
-    props: { entries },
-  };
-}
+// export async function getServerSideProps(context) {
+//   const entries = await redis.lrange("todo_log", 0, -1);
+//   return {
+//     props: { entries },
+//   };
+// }
 
-export default function ViewLog(props) {
-  return (
-    <div>
-      <h1>Api request log</h1>
-      {props.entries.map((entry) => {
-        return (
-          // eslint-disable-next-line react/jsx-key
-          <div>
-            <pre>{JSON.stringify(entry, null, 2)}</pre>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
-
-
+// export default function ViewLog(props) {
+//   return (
+//     <div>
+//       <h1>Api request log</h1>
+//       {props.entries.map((entry) => {
+//         return (
+//           // eslint-disable-next-line react/jsx-key
+//           <div>
+//             <pre>{JSON.stringify(entry, null, 2)}</pre>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }

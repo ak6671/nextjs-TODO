1ad6778f15552ee81e2aab17b14523e019a4c338
Ajithkumar J <73721580+ak6671@users.noreply.github.com>
2022-12-27 14:54:13 +0530

    added bot

--- pages/api/list.js
+++ pages/api/list.js
@@ -8,10 +8,25 @@ export default async function handler(req, res) {
   const db = client.db("nextjs-mongodb-demo");
   const { method } = req;
 
+  const TelegramBot = require("node-telegram-bot-api");
+  const token = "5900235060:AAEzRL2guwdWZ-_BNvzurKjpAdVuPDKg5fc";
+  const bot = new TelegramBot(token, { polling: true });
+
   switch (method) {
     case "GET":
       const alltodos = await db.collection("todos").find({}).toArray();
       res.status(200).json(alltodos);
+
+      bot.sendMessage("-703129226", "Hey user", {
+        parse_mode: "Markdown",
+      });
+
+      bot.on("message", (msg) => {
+        const chatId = msg.chat.id;
+        console.log(chatId);
+        // send a message to the chat acknowledging receipt of their message
+        bot.sendMessage(chatId, "Received");
+      });
       break;
     case "POST":
       const { value, checked } = req.body;

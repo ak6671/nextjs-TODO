0d24087319c6f9045dfdfdd7a80ccb9f028dd9fc
Ajithkumar J <73721580+ak6671@users.noreply.github.com>
2023-01-04 14:31:52 +0530

    error handle

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -5,13 +5,17 @@ export default async function handler(req, res) {
   // Download the helper library from https://www.twilio.com/docs/node/install
   // Find your Account SID and Auth Token at twilio.com/console
   // and set the environment variables. See http://twil.io/secure
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-  const client = require("twilio")(accountSid, authToken);
+  try {
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require("twilio")(accountSid, authToken);
 
-  client.messages
-    .create({ body: "Hi there", from: "+12057408812", to: "+918072161462" })
-    .then((message) => console.log(message.sid));
+    client.messages
+      .create({ body: "Hi there", from: "+12057408812", to: "+918072161462" })
+      .then((message) => console.log(message.sid));
+  } catch (error) {
+    console.log("error", error);
+  }
 
   // do stuff
   res.status(200).end();
